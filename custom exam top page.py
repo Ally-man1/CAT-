d4ad5ac38421882c01b8f2809c374eda4126cdbf
Ally-man1 <machariamorris12@gmail.com>
2025-05-29 19:08:16 +0300

    Update custom exam top page.py

--- custom exam top page.py
+++ custom exam top page.py
@@ -6,7 +6,7 @@ import shutil
 import pandas as pd
 from datetime import datetime
 from io import BytesIO
-from reportlab.lib.pagesizes import A4
+
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 import streamlit as st
